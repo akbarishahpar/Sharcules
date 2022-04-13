2d1a3a4bd540dd70c4f136b9fcb4c1f1342ab255
Mohsen Akbari-Shahpar <mohsen-akbari@outlook.com>
2022-04-13 15:15:22 +0430

    using relative paths

--- src/textures.ts
+++ src/textures.ts
@@ -1,5 +1,5 @@
-export const shark = "/assets/shark-128px.png";
-export const shark_flipped = "/assets/shark-128px-flipped.png";
-export const bomb = "/assets/bomb-128px.png";
-export const bomb_not_burning = "/assets/bomb-off-128px.png";
-export const fish = "/assets/fish00-128px.png";
+export const shark = "assets/shark-128px.png";
+export const shark_flipped = "assets/shark-128px-flipped.png";
+export const bomb = "assets/bomb-128px.png";
+export const bomb_not_burning = "assets/bomb-off-128px.png";
+export const fish = "assets/fish00-128px.png";
